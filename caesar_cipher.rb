1930c8455e3cba0e220cc37e336096803a98c94a
JNFussion <juan.jose.gutierrez.99@gmail.com>
2020-09-28 20:26:27 +0200

    formatting

---
+++ caesar_cipher.rb
@@ -0,0 +1,27 @@
+def caesar_cipher(str, offset)
+  code = ''
+
+  offset = 26 + offset if offset.negative?
+
+  str.each_char do |char|
+    if char =~ /[a-z]/
+
+      value = char.codepoints[0] - 97
+      value_result = (value + offset) % 26
+      code += (97 + value_result).chr
+
+    elsif char =~ /[A-Z]/
+
+      value = char.codepoints[0] - 65
+      value_result = (value + offset) % 26
+      code += (65 + value_result).chr
+
+    else
+      code += char
+    end
+  end
+  code
+end
+
+puts caesar_cipher('What a string!', 5)
+puts caesar_cipher('Bmfy f xywnsl!', -5)
